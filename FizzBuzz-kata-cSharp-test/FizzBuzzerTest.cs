34d0808e0b71ca8d5f4d95105687fb683037c8ef
Lukas Bugaj <lukas.bugaj@cas.de>
2017-07-11 22:29:45 +0200

    Initial commit - Starting point

---
+++ FizzBuzz-kata-cSharp-test/FizzBuzzerTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class FizzBuzzerTest
+    {
+        private FizzBuzzer fb = new FizzBuzzer();
+
+        [TestMethod]
+        public void RandomNumber()
+        {
+            Assert.AreEqual("4", fb.Translate(4));
+        }
+
+        [TestMethod]
+        public void Fizz()
+        {
+            Assert.AreEqual("Fizz", fb.Translate(15));
+        }
+
+        [TestMethod]
+        public void Buzz()
+        {
+            Assert.AreEqual("Buzz", fb.Translate(14));
+        }
+
+        [TestMethod]
+        public void FizzBuzz()
+        {
+            Assert.AreEqual("FizzBuzz", fb.Translate(35));
+        }
+    }
+}
---
+++ FizzBuzz-kata-cSharp/FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        public string Translate(int number)
+        {
+            if (number % 5 == 0 && number % 7 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Fizz";
+            }
+            if (number % 7 == 0)
+            {
+                return "Buzz";
+            }
+            return number.ToString();
+        }
+    }
+}
